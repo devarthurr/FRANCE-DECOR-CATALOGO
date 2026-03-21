46734d047136dd6a625f80c51a7ace3d39b2d883
empreendimentos-SA <arthurestudos18@gmail.com>
2026-03-21 13:29:43 -0300

    ccc

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
 const conteudo = document.getElementById("conteudo");
-const categoriasNav = document.getElementById("categorias");
 
-function gerarCategorias() {
-  const categorias = [...new Set(produtos.map(p => p.categoria))];
-
-  categorias.forEach(cat => {
-    const btn = document.createElement("button");
-    btn.textContent = cat;
-    btn.onclick = () => mostrarProdutos(cat);
-    categoriasNav.appendChild(btn);
-  });
-}
-
-function mostrarProdutos(categoria) {
+function mostrarProdutos() {
   conteudo.innerHTML = `<div class="catalogo"></div>`;
   const catalogo = document.querySelector(".catalogo");
 
-  produtos
-    .filter(p => p.categoria === categoria)
-    .forEach((produto, index) => {
-      catalogo.innerHTML += `
-        <div class="produto" onclick="abrirProduto(${index})">
-          <img src="${produto.imagens[0]}" class="preview">
-          <h3>${produto.nome}</h3>
-          <p class="preco">
-            ${produto.preco ? "R$ " + produto.preco : "Consultar valor"}
-          </p>
-        </div>
-      `;
-    });
+  produtos.forEach((produto, index) => {
+    catalogo.innerHTML += `
+      <div class="produto" onclick="abrirProduto(${index})">
+        <img src="${produto.imagens[0]}" class="preview">
+        <h3>${produto.nome}</h3>
+        <p class="preco">
+          ${produto.preco ? "R$ " + produto.preco : "Consultar valor"}
+        </p>
+      </div>
+    `;
+  });
 }
 
 function abrirProduto(index) {
@@ -36,9 +22,10 @@ function abrirProduto(index) {
 
   conteudo.innerHTML = `
     <div class="pagina-produto">
-      <button class="voltar" onclick="location.reload()">Voltar</button>
+      <button class="voltar" onclick="mostrarProdutos()">Voltar</button>
 
       <div class="detalhe-container">
+
         <div class="galeria">
           <img src="${produto.imagens[0]}" class="imagem-grande" id="imagemPrincipal">
 
@@ -49,7 +36,7 @@ function abrirProduto(index) {
           </div>
         </div>
 
-        <div class="info">
+        <div>
           <h2>${produto.nome}</h2>
           <p>${produto.preco ? "R$ " + produto.preco : "Consultar valor"}</p>
 
@@ -57,6 +44,7 @@ function abrirProduto(index) {
             <button class="whatsapp">Falar no WhatsApp</button>
           </a>
         </div>
+
       </div>
     </div>
   `;
@@ -66,4 +54,4 @@ function trocarImagem(src) {
   document.getElementById("imagemPrincipal").src = src;
 }
 
-gerarCategorias();
+mostrarProdutos();

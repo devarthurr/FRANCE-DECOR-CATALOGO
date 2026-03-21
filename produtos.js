39094bf0b35e17960fab170596b4b28dd0b64299
empreendimentos-SA <arthurestudos18@gmail.com>
2026-03-21 14:00:03 -0300

    asdasd

--- produtos.js
+++ produtos.js
@@ -13,4 +13,18 @@ const produtos = [
     ]
   },
 
+    {
+    nome: "TOPOS DE BOLO",
+    categoria: "FESTAS",
+    preco: "",
+    descricao: "NOSSOS TOPOS DE BOLOS",
+    imagens: [
+      "images/TOPOS DE BOLO/decoracao.jpeg",
+      "images/TOPOS DE BOLO/decoracao1.jpeg",
+      "images/TOPOS DE BOLO/decoracao2.jpeg",
+      "images/TOPOS DE BOLO/decoracao3.jpeg",
+      "images/TOPOS DE BOLO/decoracao4.jpeg"
+    ]
+  },
+
 ];
